f112b66278b9afe202e036d81a4c4189a72dcb56
loveleyz <loveleyz14@gmail.com>
2020-09-04 08:56:02 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,7 @@ DEVICE_SCREEN_HEIGHT := 1520
 
 # Display
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_DEFAULT_BRIGHTNESS := 188
+TW_DEFAULT_BRIGHTNESS := 120
 TW_MAX_BRIGHTNESS := 255
 TW_NO_SCREEN_BLANK := true
 TW_NO_BATT_PERCENT := false
@@ -128,15 +128,6 @@ TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/bootdevice/by-name/userdata"
 TW_CRYPTO_MNT_POINT := "/data"
 TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,barrier=0,data=ordered"
 
-# Storage
-TW_HAS_MTP := true
-TW_MTP_DEVICE := /dev/mtp_usb
-TW_INTERNAL_STORAGE_PATH := "/data/media/0"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-#TW_NO_REBOOT_BOOTLOADER := false
-TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_NO_USB_STORAGE := false
 TW_INPUT_BLACKLIST := "hbtp_vm"
 HAVE_SELINUX := true
